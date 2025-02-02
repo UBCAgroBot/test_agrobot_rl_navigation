4d90fd9b38a01bba7cad8e96ab8e2323a6609546
clay-arras <astrollin.neil@gmail.com>
2025-02-02 17:41:22 -0500

    klsjdflkj

--- car_obstacles.py
+++ car_obstacles.py
@@ -15,30 +15,19 @@ import pygame
 from pygame import gfxdraw
 
 
-STATE_W = 96  # less than Atari 160x192
+STATE_W = 96  
 STATE_H = 96
 VIDEO_W = 600
 VIDEO_H = 400
 WINDOW_W = 1000
 WINDOW_H = 800
-TILE_DIMS = 5
+TILE_DIMS = 5 
 
-SCALE = 20.0  # Track scale
-TRACK_RAD = 900 / SCALE  # Track is heavily morphed circle with this radius
-PLAYFIELD = 2000 / SCALE  # Game over boundary
-FPS = 50  # Frames per second
-ZOOM = 0.4  # Camera zoom
-
-
-TRACK_DETAIL_STEP = 21 / SCALE
-TRACK_TURN_RATE = 0.31
-TRACK_WIDTH = 40 / SCALE
-BORDER = 8 / SCALE
-BORDER_MIN_COUNT = 4
-GRASS_DIM = PLAYFIELD / 20.0
-MAX_SHAPE_DIM = (
-    max(GRASS_DIM, TRACK_WIDTH, TRACK_DETAIL_STEP) * math.sqrt(2) * ZOOM * SCALE
-)
+SCALE = 20.0  
+PLAYFIELD = 2000 / SCALE  
+FPS = 50 
+ZOOM = 0.4  
+MAX_SHAPE_DIM = TILE_DIMS * math.sqrt(2) * ZOOM * SCALE
 
 
 class FrictionDetector(contactListener):
@@ -69,6 +58,7 @@ class FrictionDetector(contactListener):
         if not u1 or not u2:
             return False
 
+        # road friction property is tile, tiles property is car
         if "road_friction" in u1.__dict__ and "tiles" in u2.__dict__:
             tile = u1
             obj = u2
@@ -176,14 +166,14 @@ class CarRacing(gym.Env):
         t.road_friction = 1.0
         t.color = self.end_color if is_end else self.obs_color
         t.CreateFixture(
-            fixtureDef(shape=polygonShape(box=(TILE_DIMS, TILE_DIMS)), isSensor=is_end)
+            fixtureDef(shape=polygonShape(box=(TILE_DIMS/2, TILE_DIMS/2)), isSensor=is_end)
         )
 
         vertices = [
-            (x - TILE_DIMS, y - TILE_DIMS),
-            (x + TILE_DIMS, y - TILE_DIMS),
-            (x + TILE_DIMS, y + TILE_DIMS),
-            (x - TILE_DIMS, y + TILE_DIMS),
+            (x - TILE_DIMS/2, y - TILE_DIMS/2),
+            (x + TILE_DIMS/2, y - TILE_DIMS/2),
+            (x + TILE_DIMS/2, y + TILE_DIMS/2),
+            (x - TILE_DIMS/2, y + TILE_DIMS/2),
         ]
         poly_info = (vertices, t.color)
         return t, poly_info
